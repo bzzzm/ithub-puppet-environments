2339d93a2704f4d325a26305516442fbf3bc02ad
Daniel Lupescu <dlupescu@gmail.com>
2016-12-10 15:21:50 +0200

    also create web roots for vhosts

--- modules/radar/manifests/resources/web/nginx.pp
+++ modules/radar/manifests/resources/web/nginx.pp
@@ -9,7 +9,22 @@ class radar::resources::web::nginx (
     'cdn.radar.ithub.gov.ro',
   ]
 
+  file { '/var/www':
+    ensure => directory,
+    owner  => 'nginx',
+    group  => 'nginx',
+    mode   => '0755',
+  }
+
   $vhosts.each |String $vhost| {
+    file { "/var/www/${vhost}":
+      ensure  => directory,
+      owner   => 'nginx',
+      group   => 'nginx',
+      mode    => '0755',
+      require => File['/var/www'],
+    }
+
     nginx::resource::vhost { "${vhost}":
       www_root => "/var/www/${vhost}",
     }
